f82c3924daafd15f6f43dedb7ca093b6a4f2de29
Phong Tran <tranthanhphong270997@gmail.com>
2022-04-26 16:07:45 +0700

    modify product schema

--- models/product.js
+++ models/product.js
@@ -32,57 +32,62 @@ const productOptionSchema = new mongoose.Schema({
 	},
 });
 
-const productSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		minlength: 3,
-		maxlength: 255,
-		required: true,
+const productSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			minlength: 3,
+			maxlength: 255,
+			required: true,
+		},
+		category: {
+			type: String,
+			minlength: 3,
+			maxlength: 100,
+			lowercase: true,
+			required: true,
+		},
+		subCategory: {
+			type: String,
+			minlength: 3,
+			maxlength: 100,
+			lowercase: true,
+			required: true,
+		},
+		description: {
+			type: String,
+			minlength: 3,
+			maxlength: 1000,
+			required: true,
+		},
+		basePrice: {
+			type: Number,
+			min: 0.01,
+			required: true,
+			get: (v) => (v / 100).toFixed(2),
+			set: (v) => v * 100,
+		},
+		discount: {
+			type: Number,
+			default: 0,
+			min: 0,
+			max: 90,
+			get: (v) => v * 100,
+			set: (v) => v / 100,
+		},
+		options: {
+			type: [productOptionSchema],
+			required: true,
+		},
+		sold: {
+			type: Number,
+			default: 0,
+		},
 	},
-	category: {
-		type: String,
-		minlength: 3,
-		maxlength: 100,
-		lowercase: true,
-		required: true,
-	},
-	subCategory: {
-		type: String,
-		minlength: 3,
-		maxlength: 100,
-		lowercase: true,
-		required: true,
-	},
-	description: {
-		type: String,
-		minlength: 3,
-		maxlength: 1000,
-		required: true,
-	},
-	basePrice: {
-		type: Number,
-		min: 0.01,
-		required: true,
-		get: (v) => (v / 100).toFixed(2),
-		set: (v) => v * 100,
-	},
-	discount: {
-		type: Number,
-		default: 0,
-		min: 0,
-		max: 90,
-		get: (v) => v * 100,
-		set: (v) => v / 100,
-	},
-	options: {
-		type: [productOptionSchema],
-		required: true,
-	},
-	sold: {
-		type: Number,
-		default: 0,
-	},
-});
+	{
+		toJSON: { getters: true },
+	}
+);
 
 const Product = mongoose.model("Product", productSchema);
 
@@ -95,11 +100,14 @@ const validateProduct = (obj) => {
 		basePrice: Joi.number().min(0).required(),
 		discount: Joi.number().min(0).max(90).required(),
 		options: Joi.array().items({
+			img: Joi.array().items(Joi.string()),
 			color: Joi.string().required(),
 			quantityPerSize: Joi.array().items({
 				size: Joi.string().min(1).max(5).required(),
 				quantity: Joi.number().min(0).required(),
+				_id: Joi.objectId(),
 			}),
+			_id: Joi.objectId(),
 		}),
 	});
 
